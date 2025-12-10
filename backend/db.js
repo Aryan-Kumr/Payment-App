dfa160bdb4626f270f83a14d546d18783c4399f7
Aryan-Kumr <152487056+Aryan-Kumr@users.noreply.github.com>
2025-12-10 15:37:00 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb+srv://admin:NQTRj8MZl5o88N2o@cluster0.7qkjke1.mongodb.net/paytm") //YOUR CONNECTION URL
+mongoose.connect("mongodb+srv://admin:NQTmongodb.net/paytm") //YOUR CONNECTION URL
 
 
 const userSchema = new mongoose.Schema({
