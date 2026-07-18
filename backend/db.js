354b552c065824a73bbc9682923ffdd6c53b406a
Aryan-Kumr <aryann1077@gmail.com>
2026-07-18 15:47:43 +0530

    example mongodb connection url added

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require ("mongoose");
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
-// mongoose.connect("mongodb+srv://admin:NQTRj8MZl5o88N2o@cluster0.7qkjke1.mongodb.net/tablename") //YOUR CONNECTION URL
+// mongoose.connect("mongodb+srv://<username>:<password>@cluster0.7qkjke1.mongodb.net/tablename") //YOUR CONNECTION URL
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB connected successfully");
